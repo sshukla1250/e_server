1cce5a940f9143631a3e008f0557a7361ebd5e4f
Sandeep Shukla <82631670+sshukla1250@users.noreply.github.com>
2024-11-06 02:32:13 +0530

    Update EServerApplication.java
    change in java file

--- src/main/java/com/eserver/EServerApplication.java
+++ src/main/java/com/eserver/EServerApplication.java
@@ -6,10 +6,18 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RequestMapping("/message")
 public class EServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EServerApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String getServiceInfo() {
+		return "service name: Eserver application";
+	}
+	
 
 }
